9d69fc15320f9cfbd9adf95ae74c1249c83d8413
Panos Oikonomou <61333112+eigenP@users.noreply.github.com>
2024-01-10 18:50:49 -0500

    Update maxproj_registration.py

--- maxproj_registration.py
+++ maxproj_registration.py
@@ -49,7 +49,35 @@ def zero_shift_multi_dimensional(arr, shifts = 0, fill_value=0):
     result[tuple(slices_output)] = arr[tuple(slices_input)]
     return result
 
-def estimate_drift_2D(frame1, frame2, _UPSAMPLE_FACTOR = 8, return_ccm = False):
+def _2D_weighted_image(image, overlap):
+    '''
+    # image := image shape
+    # overlap := in pixels
+
+    # Example usage
+    # _2D_window = _2D_weight(image, overlap)
+    '''
+
+    # 1D weight function based on cubic spline
+    def weight_1d(x):
+        return 3 * x**2 - 2 * x**3
+
+    # Initialize weight matrix with ones
+    weight_2d = np.ones_like(image, dtype = np.float16)
+
+    # Apply weight function to the top, bottom, left, and right overlap regions
+    for i in range(overlap):
+        weight = weight_1d(i / (overlap - 1))
+        weight_2d[i, :] *= weight
+        weight_2d[-(i + 1), :] *= weight
+        weight_2d[:, i] *= weight
+        weight_2d[:, -(i + 1)] *= weight
+
+    weighted_image = image * weight_2d
+
+    return weighted_image
+
+def estimate_drift_2D(frame1, frame2, return_ccm = False):
     """
     Estimate the xy-drift between two 2D frames using cross-correlation.
 
@@ -59,6 +87,13 @@ def estimate_drift_2D(frame1, frame2, _UPSAMPLE_FACTOR = 8, return_ccm = False):
     """
     # Calculate the cross-correlation matrix
     # shift, error, diffphase = phase_cross_correlation(frame1, frame2)
+
+    min_size_pixels = min(frame1.shape)
+
+    frame1 = _2D_weighted_image(frame1, min_size_pixels // 3)
+    frame2 = _2D_weighted_image(frame2, min_size_pixels // 3)
+
+
     frame1_max_proj_x = np.max(frame1, axis = 0)
     frame2_max_proj_x = np.max(frame2, axis = 0)
 
@@ -72,17 +107,13 @@ def estimate_drift_2D(frame1, frame2, _UPSAMPLE_FACTOR = 8, return_ccm = False):
     # frame1_max_proj_y = gaussian_filter1d(frame1_max_proj_y, sigma = 3, radius = 5)
     # frame2_max_proj_y = gaussian_filter1d(frame2_max_proj_y, sigma = 3, radius = 5)
 
-    
+
 
     shift_x, error, diffphase = phase_cross_correlation(frame1_max_proj_x,
-                                                      frame2_max_proj_x,
-                                                      upsample_factor = _UPSAMPLE_FACTOR,
-                                                      overlap_ratio= 0.4)
+                                                      frame2_max_proj_x)
 
     shift_y, error, diffphase = phase_cross_correlation(frame1_max_proj_y,
-                                                      frame2_max_proj_y,
-                                                      upsample_factor = _UPSAMPLE_FACTOR,
-                                                      overlap_ratio= 0.4)
+                                                      frame2_max_proj_y)
 
     shift = np.array((shift_x[0], shift_y[0]))
 
@@ -122,8 +153,7 @@ def estimate_drift_2D(frame1, frame2, _UPSAMPLE_FACTOR = 8, return_ccm = False):
     else:
         return shift
 
-
-def apply_drift_correction_2D(video_data, save_drift_table=False, csv_filename='drift_table.csv'):
+def apply_drift_correction_2D(video_data, reverse_time = False, save_drift_table=False, csv_filename='drift_table.csv'):
     """
     Apply drift correction to video data.
 
@@ -139,16 +169,13 @@ def apply_drift_correction_2D(video_data, save_drift_table=False, csv_filename='
         - corrected_data: A 3D numpy array of the same shape as video_data, representing the drift-corrected video.
         - drift_table: A pandas DataFrame containing the drift values, cumulative drift, and time points.
     """
-
-
     # Get the dimensions of the video data
-    t, x, y = video_data.shape
+    t_shape, x_shape, y_shape = video_data.shape
 
     # Initialize an array to store the corrected video data
     corrected_data = np.zeros_like(video_data)
 
-    # The first frame does not need correction
-    corrected_data[0] = video_data[0]
+
 
     # Initialize variables to store cumulative drift
     cum_dx, cum_dy = 0, 0
@@ -161,29 +188,87 @@ def apply_drift_correction_2D(video_data, save_drift_table=False, csv_filename='
 
     # Loop through each time point in the video data, starting from the second frame
     # Wrap the range function with tqdm for a progress bar
-    for time_point in tqdm(range(1, t), desc='Applying Drift Correction'):
-    # for time_point in range(1, t):
-        # Estimate the drift between the current frame and the previous frame
-        dx, dy = estimate_drift_2D(video_data[time_point - 1], video_data[time_point])
 
-        ##### if too large, then keep as zero ####
-        if abs(dx) > x//4:
-            dx = 0
-        if abs(dy) > y//4:
-            print('Whaa')
-            dy = 0
+    if reverse_time == 'both':
+        # Reverse order
+        range_values = range(t_shape - 1, 0, -1)
 
-        # Update the cumulative drift
-        cum_dx, cum_dy = int(cum_dx + dx), int(cum_dy + dy)
 
-        # Apply drift correction to the current frame
-        corrected_data[time_point] = zero_shift_multi_dimensional(video_data[time_point], shifts=(cum_dy, cum_dx), fill_value = min_value)
 
-        # Record the drift values and cumulative drift for the current time point
-        drift_records.append({'Time Point': time_point, 'dx': dx, 'dy': dy, 'cum_dx': cum_dx, 'cum_dy': cum_dy})
+        for time_point in tqdm(range_values, desc='Applying Drift Correction'):
+            # for time_point in range(1, t):
+            # Estimate the drift between the current frame and the previous frame
+            dx_backward, dy_backward = estimate_drift_2D(video_data[time_point - 1], video_data[time_point])
+            dx_backward, dy_backward = dx_backward, dy_backward
+
+            dx_forward, dy_forward = estimate_drift_2D(video_data[time_point], video_data[time_point-1])
+            dx_forward, dy_forward = dx_forward, dy_forward
+
+
+            # print(dx_forward, dx_backward)
+            dx = (dx_forward - dx_backward) / 2
+            dy = (dy_forward - dy_backward) / 2
+
+
+            ##### if too large, then keep as zero ####
+            if abs(dx) > x_shape//5:
+                dx = 0
+            if abs(dy) > y_shape//5:
+                print('Whaa')
+                dy = 0
+
+            # Update the cumulative drift
+            cum_dx, cum_dy = int(cum_dx + dx), int(cum_dy + dy)
+
+            # Apply drift correction to the current frame
+            OFFSET = 1 if reverse_time else 0 
+            corrected_data[time_point] = zero_shift_multi_dimensional(video_data[time_point - OFFSET], shifts=(cum_dy, cum_dx), fill_value = min_value)
+
+            # Record the drift values and cumulative drift for the current time point
+            drift_records.append({'Time Point': time_point, 'dx': dx, 'dy': dy, 'cum_dx': cum_dx, 'cum_dy': cum_dy})
+    else:
+
+        if not reverse_time:
+            # Regular order
+            range_values = range(1, t_shape)
+            DRIFT_SIGN = 1
+
+            # The first frame does not need correction
+            corrected_data[0] = video_data[0]
+        else:
+            # Reversed order
+            range_values = range(t_shape - 1, 0, -1)
+            DRIFT_SIGN = -1
+
+            # The first frame does not need correction
+            corrected_data[-1] = video_data[-1]
+
+        for time_point in tqdm(range_values, desc='Applying Drift Correction'):
+            # for time_point in range(1, t):
+            # Estimate the drift between the current frame and the previous frame
+            dx, dy = estimate_drift_2D(video_data[time_point - 1], video_data[time_point])
+            dx, dy = dx * DRIFT_SIGN, dy * DRIFT_SIGN
+
+            ##### if too large, then keep as zero ####
+            if abs(dx) > x_shape//5:
+                dx = 0
+            if abs(dy) > y_shape//5:
+                print('Whaa')
+                dy = 0
+
+            # Update the cumulative drift
+            cum_dx, cum_dy = int(cum_dx + dx), int(cum_dy + dy)
+
+            # Apply drift correction to the current frame
+            OFFSET = 1 if reverse_time else 0 
+            corrected_data[time_point] = zero_shift_multi_dimensional(video_data[time_point - OFFSET], shifts=(cum_dy, cum_dx), fill_value = min_value)
+
+            # Record the drift values and cumulative drift for the current time point
+            drift_records.append({'Time Point': time_point, 'dx': dx, 'dy': dy, 'cum_dx': cum_dx, 'cum_dy': cum_dy})
 
     # Create a DataFrame from the list of drift records
     drift_table = pd.DataFrame(drift_records)
+    drift_table.sort_values(by=['Time Point'], inplace=True)
 
     # Optionally, save the drift table to a CSV file
     if save_drift_table:
