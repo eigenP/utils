611ace88e3605ee2680fcf69e964049af568857c
Panos Oikonomou <61333112+eigenP@users.noreply.github.com>
2026-01-09 18:33:54 -0500

    Change render mode from 'density' to 'points'

--- src/eigenp_utils/tnia_plotting_anywidgets.py
+++ src/eigenp_utils/tnia_plotting_anywidgets.py
@@ -203,7 +203,7 @@ class TNIASliceWidget(TNIAWidgetBase):
 
 
 class TNIAScatterWidget(TNIAWidgetBase):
-    def __init__(self, X_arr, Y_arr, Z_arr, channels=None, sxy=1, sz=1, render='density', bins=512,
+    def __init__(self, X_arr, Y_arr, Z_arr, channels=None, sxy=1, sz=1, render='points', bins=512,
                  point_size=4, alpha=0.6, colors=None, gamma=1, vmin=None, vmax=None, figsize=None,
                  x_s=None, y_s=None, z_s=None, x_t=None, y_t=None, z_t=None):
 
